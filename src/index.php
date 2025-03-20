197f698260ca83673afbfbb554665341427e9955
Luksssssssss <lukasis289@gmail.com>
2025-03-20 22:09:13 +0200

    Feat: add sorting logic

--- src/index.php
+++ src/index.php
@@ -4,3 +4,5 @@
     $numbers = file_exists($file_path) ? array_map('intval', file($file_path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES))
     : die("Error: File not found!");
     
+    sort($numbers); 
+
