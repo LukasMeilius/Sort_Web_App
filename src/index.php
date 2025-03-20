0f290aee2940b10337de82d93533dc07dd21a133
Luksssssssss <lukasis289@gmail.com>
2025-03-20 22:08:02 +0200

    Feat: add file reading and error handling for missing file

---
+++ src/index.php
@@ -0,0 +1,6 @@
+<?php
+    $file_path = '../data/input.txt';
+
+    $numbers = file_exists($file_path) ? array_map('intval', file($file_path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES))
+    : die("Error: File not found!");
+    
