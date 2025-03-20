165646e3706800997e273276ed1619736ee21783
Luksssssssss <lukasis289@gmail.com>
2025-03-20 22:18:01 +0200

    Feat: add HTML structure to display sorted numbers

--- src/index.php
+++ src/index.php
@@ -12,3 +12,19 @@
     $sorted_numbers[] = "<li>".htmlspecialchars($num)."</li>" ;
 }
         
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Sorting_task</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Sorted numbers</h1>
+        <ul> 
+            <?php echo join($sorted_numbers); ?>    
+        </ul>
